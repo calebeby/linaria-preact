14c146c88d4975a174a30cfcea46ec05327f6518
Caleb Eby <caleb.eby01@gmail.com>
2019-05-07 22:17:48 -0700

    Make it allow imports to linaria-styled-preact

--- src/babel/utils/hasImport.js
+++ src/babel/utils/hasImport.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-import { dirname } from 'path';
+import { dirname } from 'path'
 
 // Verify if the binding is imported from the specified source
 export default function hasImport(
@@ -10,28 +10,28 @@ export default function hasImport(
   identifier: string,
   source: string
 ) {
-  const binding = scope.getAllBindings()[identifier];
+  const binding = scope.getAllBindings()[identifier]
 
   if (!binding) {
-    return false;
+    return false
   }
 
-  const p = binding.path;
+  const p = binding.path
 
   const resolveFromFile = id => {
     /* $FlowFixMe */
-    const M = require('module');
+    const M = require('module')
 
     try {
       return M._resolveFilename(id, {
         id: filename,
         filename,
         paths: M._nodeModulePaths(dirname(filename)),
-      });
+      })
     } catch (e) {
-      return null;
+      return null
     }
-  };
+  }
 
   const isImportingModule = value =>
     // If the value is an exact match, assume it imports the module
@@ -43,10 +43,10 @@ export default function hasImport(
         ? require.resolve('../../index')
         : source === 'linaria/react'
         ? require.resolve('../../react/')
-        : resolveFromFile(source));
+        : resolveFromFile(source))
 
   if (t.isImportSpecifier(p) && t.isImportDeclaration(p.parentPath)) {
-    return isImportingModule(p.parentPath.node.source.value);
+    return isImportingModule(p.parentPath.node.source.value)
   }
 
   if (t.isVariableDeclarator(p)) {
@@ -56,17 +56,17 @@ export default function hasImport(
       p.node.init.callee.name === 'require' &&
       p.node.init.arguments.length === 1
     ) {
-      const node = p.node.init.arguments[0];
+      const node = p.node.init.arguments[0]
 
       if (t.isStringLiteral(node)) {
-        return isImportingModule(node.value);
+        return isImportingModule(node.value)
       }
 
       if (t.isTemplateLiteral(node) && node.quasis.length === 1) {
-        return isImportingModule(node.quasis[0].value.cooked);
+        return isImportingModule(node.quasis[0].value.cooked)
       }
     }
   }
 
-  return false;
+  return false
 }
